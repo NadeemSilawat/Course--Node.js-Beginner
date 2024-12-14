ea27e554870b422cff42f3439cc768b63542acc4
NadeemSilawat <im.nadeem.silawat@gmail.com>
2024-12-14 20:30:22 +0530

    Module-Export Vs Export

---
+++ Tut-1/06-module-exports vs export/object-reference.js
@@ -0,0 +1,24 @@
+// Pattern 1 
+
+// const obj1 = {
+//     name : "Bruce Wayve"
+// }
+
+// const obj2 = obj1;
+
+// obj2.name = "Mynyash";
+// console.log(obj1)
+
+
+// Pattern 2
+
+const obj1 = {
+    name : "Bruce Wayve"
+}
+
+let obj2 = obj1;
+
+obj2 = {
+    name : "Mynd",
+} 
+console.log(obj1);
